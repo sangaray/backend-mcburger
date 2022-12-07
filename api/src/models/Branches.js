c28667934f5134419f93cb4ac7b213e54796ac93
Sandra Garaycochea <scgaraycochea@yahoo.com.ar>
2022-12-07 20:59:20 -0300

    creacion de models

---
+++ api/src/models/Branches.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("branches", {
+    station: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    localityId:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false,
+  }
+    );
+};
