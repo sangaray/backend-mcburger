83c9839824da507fbc17c1bf24414870f3d4dd01
Cristian-tec <crgs2008@gmail.com>
2022-12-07 20:44:58 -0300

    models

---
+++ api/src/models/Products.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('products', {
+        /*  uiD:{
+           type: DataTypes.UUID,
+           defaultValue: DataTypes.UUIDV4,
+           primaryKey: true,
+         } */
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        ingredients: {
+            type: DataTypes.STRING
+        },
+        summary: {
+            type: DataTypes.TEXT
+        },
+        price: {
+            type: DataTypes.STRING
+        },
+        idBranch: {
+            type: DataTypes.INTEGER
+        },
+        idCategory: {
+            type: DataTypes.INTEGER
+        },
+        image: {
+            type: DataTypes.TEXT
+        }
+    },
+        {
+            timestamps: false,
+        }
+    );
+};
+
