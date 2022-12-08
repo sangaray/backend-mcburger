987ee835870efa5e491018b73ec0f9950eeca236
Sandra Garaycochea <scgaraycochea@yahoo.com.ar>
2022-12-07 22:13:20 -0300

    agregado relaciones

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,18 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe } = sequelize.models;
+const {User, Localities, Branches, Products, Categories} = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Localities.hasMany(User);
+User.belongsTo(Localities);
+
+Localities.hasMany(Branches);
+Branches.belongsTo(Localities);
+
+Categories.hasMany(Products);
+Products.belongsTo(Categories);
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
