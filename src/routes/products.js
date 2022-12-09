3a819561ccabe251b5d24d2a5568ea6b3d5ce599
Frankroz <franjrm20119@gmail.com>
2022-12-08 22:19:02 -0500

    Crear filtro ingredients GET/products?ingredient=

--- src/routes/products.js
+++ src/routes/products.js
@@ -9,31 +9,36 @@ const router = Router();
 
 router.get("/", async (req, res) => {
   // Check for params in the query
-  const name = req.query.name;
-  const category = req.query.category;
+  const { name, category, ingredient } = req.query;
 
   try {
     // Get all the products from the DB
-    const products = await searchProducts();
+    let products = await searchProducts();
 
+    // Combine filters depending on the queries
     // Check if category exists
     if (category) {
-      return res.json(
-        products.filter((product) =>
-          product.idCategory == parseInt(category))
-        );
-    }
-    // Check if name exists
-    else if (name) {
-      // If it does, search from the DB, and return them
-      return res.json(
-        products.filter((product) =>
-          product.name.toLowerCase().includes(name.toLowerCase())
-        )
+      products = products.filter(
+        (product) => product.idCategory == parseInt(category)
       );
     }
 
-    // Otherwise return all products
+    // Check if ingredient exists
+    if (ingredient) {
+      products = products.filter((product) =>
+        product.ingredients.toLowerCase().includes(ingredient.toLowerCase())
+      );
+    }
+
+    // Check if name exists
+    if (name) {
+      // If it does, search from the DB, and return them
+      products = products.filter((product) =>
+        product.name.toLowerCase().includes(name.toLowerCase())
+      );
+    }
+
+    // Return all products after the filters
     return res.status(200).json(products);
   } catch (e) {
     return res.status(404).send(e.message);
