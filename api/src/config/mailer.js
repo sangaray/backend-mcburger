946f8e74cd10a5587d5d7f670adad433a411661f
Agustina Monzón <agusdlmonzon@gmail.com>
2022-12-23 16:58:48 -0300

    mailer

---
+++ api/src/config/mailer.js
@@ -0,0 +1,44 @@
+const { User } = require("../models/user");
+const nodemailer = require("nodemailer");
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: "mcburger.pf@gmail.com", // generated ethereal user
+    pass: "txorzsvxhsvibpeo", // generated ethereal password
+  },
+});
+transporter.verify().then(() => {
+  console.log("Ready por send emails.");
+});
+
+module.exports = { transporter };
+
+//const { transporter } = require("../config/mailer");
+// // Mail de bienvenida
+// const sendEmail = async (email) => {
+//   await transporter.sendMail({
+//     from: '"Mc Burger 🍔" <foo@example.com>', // sender address
+//     to: email, // list of receivers
+//     subject: "Welcome to McBurger!", // Subject line
+//     text: "Thank you! We are delighted that you are part of our community" // plain text body
+//     html: "<b>When you register you will be sent discounts😊</b>", // html body
+//   });
+// };
+
+// // Mail decompra exitosa
+// if (payment_status === "approved") {
+// const sendEmail = async (email) => {
+//   await transporter.sendMail({
+//     from: '"Mc Burger 🍔" <foo@example.com>', // sender address
+//     to: email, // list of receivers
+//     subject: "¡Thanks for your purchase!", // Subject line
+//     text: "Thank you for choosing us. We are delighted that you are part of our community", // plain text body
+//     html: "<b>Enjoy the best flavors 😊</b>", // html body
+//   });
+// };
+// } else if (payment_status === "Cancelled") {
+
+// }
