e5dbab919c87961fa94da954a99882bf57a0a8c5
Agustina Monzón <agusdlmonzon@gmail.com>
2022-12-29 20:09:11 -0300

    funcionalidad mailer

--- api/src/routes/mercadoPago.js
+++ api/src/routes/mercadoPago.js
@@ -2,16 +2,6 @@ const { Router } = require("express");
 const router = Router();
 const { createPayment } = require("../controllers/payment_Controller");
 
-router.post("/", async (req, res) => {
-  const data = req.body;
-  console.log(data);
-  try {
-    const response = await createPayment(data);
-    console.log(response);
-    res.status(200).json(response);
-  } catch (e) {
-    return res.status(404).send({ e: e.message });
-  }
-});
+router.post("/", createPayment);
 
 module.exports = router;
